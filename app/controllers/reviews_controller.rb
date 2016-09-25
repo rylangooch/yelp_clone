1261fa06422a9c5cd76017eb824dc76b23fca1cd
Rylan Gooch <rylangooch@gmail.com>
2016-09-25 16:51:50 +0100

    Merge conflicts resolved

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,9 +4,6 @@ class ReviewsController < ApplicationController
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
-<<<<<<< HEAD
-  
-=======
 
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
@@ -20,5 +17,4 @@ class ReviewsController < ApplicationController
     params.require(:review).permit(:thoughts, :rating)
   end
 
->>>>>>> c38fd22d2e595f409b5b4e3f0c95db55657d489f
 end
